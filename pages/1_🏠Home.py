16c41e5244ff12f323174c233ca779c3556f76d6
Siddharth Gupta <siddharth18official@gmail.com>
2023-11-02 14:22:44 +0530

    Update 1_🏠Home.py xlsx file upload option removed

--- pages/1_🏠Home.py
+++ pages/1_🏠Home.py
@@ -8,12 +8,12 @@ from session_state import initialize_session_state, set_data
 def home():
     st.set_page_config(page_title="Diagnostic Analysis Tool", page_icon=":bar_chart:", layout="wide")
     st.title("Welcome to Diagnostic Analysis Tool!")
-    st.write("This tool lets you quickly analyse your data and gain insights to inform your decision-making. Simply upload your CSV file using the 'Browse files' button below to get started. Once your data is loaded, you can use the various features and tools to visualise and explore your data, including generating descriptive statistics, creating charts and graphs, finding influencing factors and more.") 
+    st.write("This tool lets you quickly analyse your data and gain insights to inform your decision-making. Upload your CSV file using the 'Browse files' button below. Once your data is loaded, you can use the various features and tools to visualise and explore your data, including generating descriptive statistics, creating charts and graphs, finding influencing factors and more.") 
     st.write("Our user-friendly interface makes it easy to work with your data, even if you're new to data analysis. Start using our Diagnostic Analysis application today and unlock the full potential of your data!")    
     image = Image.open("images/types.jpeg")
     new_image = image.resize((900,500))
     st.image(new_image)
-    uploaded_file = st.file_uploader("Upload your dataset here ✅", type=["csv", "xlsx"])
+    uploaded_file = st.file_uploader("Upload your dataset here ✅", type=["csv"])
     
     # Check if a file has been uploaded
     if uploaded_file is not None:
